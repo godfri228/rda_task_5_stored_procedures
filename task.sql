c765449fcd57c1dc27599c59a4a96b74b043c9ce
godfri228 <overe14@gmail.com>
2026-06-01 16:57:00 +0300

    Include full schema and DROP DATABASE in task.sql

--- task.sql
+++ task.sql
@@ -1,6 +1,37 @@
 DROP DATABASE IF EXISTS ShopDB;
-CREATE DATABASE IF NOT EXISTS ShopDB;
+CREATE DATABASE ShopDB;
 USE ShopDB;
+
+CREATE TABLE Countries (
+  ID INT,
+  Name VARCHAR(50),
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE Products (
+  ID INT AUTO_INCREMENT,
+  Name VARCHAR(50),
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE Warehouses (
+  ID INT AUTO_INCREMENT,
+  Name VARCHAR(50),
+  Address VARCHAR(50),
+  CountryID INT,
+  FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE ProductInventory (
+  ID INT,
+  ProductID INT,
+  FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+  WarehouseAmount INT,
+  WarehouseID INT,
+  FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
+  PRIMARY KEY (ID)
+);
 
 DROP PROCEDURE IF EXISTS get_warehouse_product_inventory;
 
