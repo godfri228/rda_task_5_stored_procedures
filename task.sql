31bdd8661afc6f1374739882ebb5cccd32550b2d
godfri228 <overe14@gmail.com>
2026-06-01 16:59:25 +0300

    Add data inserts and full schema to task.sql

--- task.sql
+++ task.sql
@@ -33,6 +33,14 @@ CREATE TABLE ProductInventory (
   PRIMARY KEY (ID)
 );
 
+INSERT INTO Countries (ID, Name) VALUES (1, 'Country1');
+INSERT INTO Countries (ID, Name) VALUES (2, 'Country2');
+INSERT INTO Warehouses (ID, Name, Address, CountryID) VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1);
+INSERT INTO Warehouses (ID, Name, Address, CountryID) VALUES (2, 'Warehouse-2', 'City-2, Street-2', 2);
+INSERT INTO Products (ID, Name) VALUES (1, 'AwersomeProduct');
+INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID) VALUES (1, 1, 2, 1);
+INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID) VALUES (2, 1, 4242, 2);
+
 DROP PROCEDURE IF EXISTS get_warehouse_product_inventory;
 
 DELIMITER //
